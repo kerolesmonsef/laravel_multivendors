cf1dfcea19945e3cab4e1123e709446478db7e3c
keroles-star <59221288+keroles-star@users.noreply.github.com>
2020-11-07 13:28:02 +0200

    delete unwanted files

--- Modules/Admin/Resources/views/dashboard.blade.php
+++ Modules/Admin/Resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master')
+@extends('admin::layout.master')
 
 @push('plugin-styles')
     <link href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet"/>
